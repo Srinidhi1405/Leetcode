ff41ec455d583f71f0276666d071159857f1db7c
Srinidhi <162612507+Srinidhi1405@users.noreply.github.com>
2026-07-30 10:35:02 +0530

    Time: 3 ms (88.31%) | Memory: 13.2 MB (28.91%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    //bool isVowel(char ch){
-      //  return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
-    //}
     int maxVowels(string s, int k) {
-        int freq[26]={0};
         int left=0;
         int vowelCount=0;
         int ans=0;
